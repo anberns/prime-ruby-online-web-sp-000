f0c3f680cf503ad0e5f111dda9c78df9bc20a55d
Aaron Berns <anberns@gmail.com>
2019-01-09 13:24:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,30 +1,25 @@
-require 'benchmark'
-
-
 # prime
 def prime?(num)
-  puts benchmark.measure{
-    if num == 2 || num == 3
-      return true 
-    elsif num == 1 
-      return false
-    end
-    
-    primes = [2,3]
-    i = 4
-    while i <= num
-    is_prime = true
-      primes.each do |prime|
-        if i % prime == 0 
-          is_prime = false 
-          break
-        end
+  if num == 2 || num == 3
+    return true 
+  elsif num == 1 
+    return false
+  end
+  
+  primes = [2,3]
+  i = 4
+  while i <= num
+  is_prime = true
+    primes.each do |prime|
+      if i % prime == 0 
+        is_prime = false 
+        break
       end
-      primes << i if is_prime == true 
-      i += 1
     end
-    primes[-1] == num ? result = true : result = false
-  }
+    primes << i if is_prime == true 
+    i += 1
+  end
+  primes[-1] == num ? result = true : result = false
   result
 end
     
