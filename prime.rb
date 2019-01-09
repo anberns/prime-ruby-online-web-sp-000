9a2da643c0738a0a1d8f710339d49a6b00ff8e99
Aaron Berns <anberns@gmail.com>
2019-01-09 13:11:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+# prime
+def prime(num)
+  if num == 1 || num == 2 || num == 3
+    return true 
+  end
+  
+  primes = [2,3]
+  i = 4
+  while i <= num
+    is_prime = false 
+    primes.each do |prime|
+      if i % prime == 0 
+        is_prime = true
+        break
+      end
+    end
+    primes << i if is_prime == true 
+    i += 1
+  end
+  puts primes
+  primes[-1] == num ? true : false
+end
+
+prime(10)
+    
